7e787ef42a2065ab249a66e915d24a4e045e0dd2
Jirch <jirchgot@gmail.com>
2025-02-04 20:03:45 -0700

    Created Refrigerator class

---
+++ AssignmentClasssesAndInheritance/Refrigerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentClasssesAndInheritance
+{
+    public class Refrigerator
+    {
+        public int ItemNumber { get; set; }
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public double Wattage { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+        public int Doors { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+    }
+}
