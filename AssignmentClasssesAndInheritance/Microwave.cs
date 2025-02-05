de05029c87d1c895db20e719ebeddf08952598c6
Jirch <jirchgot@gmail.com>
2025-02-04 20:06:32 -0700

    Created Microwave class

---
+++ AssignmentClasssesAndInheritance/Microwave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentClasssesAndInheritance
+{
+    public class Microwave
+    {
+        public int ItemNumber { get; set; }
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public double Wattage { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+        public double Capacity { get; set; }
+        public double RoomType { get; set; }
+    }
+}
