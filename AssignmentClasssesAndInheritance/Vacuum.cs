265af7c0319ced30e98444afa6756a1b9b101f14
Jirch <jirchgot@gmail.com>
2025-02-04 20:05:17 -0700

    Created Vacuum Class

---
+++ AssignmentClasssesAndInheritance/Vacuum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentClasssesAndInheritance
+{
+    public class Vacuum
+    {
+        public int ItemNumber { get; set; }
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public double Wattage { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+        public string Grade { get; set; }
+        public double Voltage { get; set; }
+    }
+}
